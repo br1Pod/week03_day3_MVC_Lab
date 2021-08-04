10e8d08dd0f775594f660ab27b6b19373d9f1832
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-08-04 15:52:51 +0100

    exstension complete, indivdual order pages

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,11 @@
 from app import app
 from flask import render_template
-from models.orders import orders_list
+from models.orders import *
 
-@app.route('/')
+@app.route('/orders')
 def index():
     return render_template('index.html', orders = orders_list)
+
+@app.route("/orders/<num>")
+def order(num):
+    return render_template('order.html', order = orders_list[int(num)])
