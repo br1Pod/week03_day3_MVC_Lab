557c7a46ec1f0d795ba4face78224f6ce5b6aa05
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-08-04 14:36:06 +0100

    fixed flask and orders import

--- models/orders.py
+++ models/orders.py
@@ -1,4 +1,4 @@
-from order_class import Order
+from models.order_class import Order
 
 order1 = Order("AA01","John Smoogs", "04/08/21","IT","Steven King", 1)
 order2 = Order("AA02", "Jean Blogs", "03/08/21","Life of Pi", "Pi",2)
