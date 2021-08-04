84dfa98bf0de2d4563500462b309cda57b96227e
NateDawggWoof <nathan.pianu@hotmail.co.uk>
2021-08-04 15:03:37 +0100

    MVP completed

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,7 +1,7 @@
 from app import app
 from flask import render_template
-from models.orders import *
+from models.orders import orders_list
 
 @app.route('/')
 def index():
-    return render_template('index.html', orders = orders)
+    return render_template('index.html', orders = orders_list)
--- models/orders.py
+++ models/orders.py
@@ -4,4 +4,4 @@ order1 = Order("AA01","John Smoogs", "04/08/21","IT","Steven King", 1)
 order2 = Order("AA02", "Jean Blogs", "03/08/21","Life of Pi", "Pi",2)
 order3 = Order("AA03","Jeff Bridges","30/07/21","Zen","Buddha",15)
 
-orders = [order1,order2,order3]
+orders_list = [order1,order2,order3]
